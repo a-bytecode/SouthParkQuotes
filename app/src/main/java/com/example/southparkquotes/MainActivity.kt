722839681fde3fb924a7892dfcf746afc76bb664
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-08-24 13:38:03 +0200

    build: Navigation-Drawer wieder entfernt.

--- app/src/main/java/com/example/southparkquotes/MainActivity.kt
+++ app/src/main/java/com/example/southparkquotes/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.southparkquotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
--- app/src/main/java/com/example/southparkquotes/UI/HomeFragment.kt
+++ app/src/main/java/com/example/southparkquotes/UI/HomeFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.example.southparkquotes.R
 import com.example.southparkquotes.databinding.HomeFragmentBinding
 import com.example.southparkquotes.model.MainViewModel
 
