25ce099f530e117d098375261eed49eb9d0a658e
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-09-05 22:31:20 +0200

    build: Beim verlassen des Frames wird nun derletzte quotes durch die resume Methode wieder gelöscht.

--- app/src/main/java/com/example/southparkquotes/UI/DetailQuoteFragment.kt
+++ app/src/main/java/com/example/southparkquotes/UI/DetailQuoteFragment.kt
@@ -38,13 +38,15 @@ class DetailQuoteFragment : Fragment() {
         return binding.root
     }
 
-    override fun onDestroyView() {
-        // Leeren Sie den Text in Ihrer TextView, wenn das Fragment zerstört wird
+    override fun onResume() {
+        super.onResume()
+
+        // Leeren Sie den Text in Ihrer TextView
         binding.detailSPQuote.text = ""
 
         // Blenden Sie die CardView aus
         binding.cardViewDetail.visibility = View.GONE
-        super.onDestroyView()
+        binding.mrHanky.alpha = 1f
     }
 
     @RequiresApi(Build.VERSION_CODES.Q)
@@ -63,17 +65,23 @@ class DetailQuoteFragment : Fragment() {
             binding.charPic01detail.setImageResource(imageResource)
         }
 
+        binding.mrHanky.alpha = 1f
+        binding.mrHanky.startAnimation(animation)
+
         viewModel.apiStatus.observe(viewLifecycleOwner) {
             when (it) {
                 ApiStatus.LOADING -> {
                     binding.cardViewDetail.visibility = View.INVISIBLE
+                    binding.mrHanky.visibility = View.GONE
                 }
                 ApiStatus.START -> {
                     binding.cardViewDetail.visibility = View.VISIBLE
+                    binding.mrHanky.alpha = 0f
                 }
                 ApiStatus.ERROR -> {
                     binding.cardViewDetail.visibility = View.GONE
-                }
+                    binding.mrHanky.alpha = 0f
+                 }
             }
         }
 
@@ -82,13 +90,17 @@ class DetailQuoteFragment : Fragment() {
             val nextQuote = viewModel.getNextQuote(binding.detailSPQuote)
             if (nextQuote != null) {
                 binding.detailSPQuote.text = nextQuote
+                binding.cardViewDetail.alpha = 1f // Setzen Sie die CardView auf sichtbar
+                binding.mrHanky.alpha = 0f
             }
             Log.d("imageName001", "${imageName}")
         }
 
         viewModel.characterNameLiveData.observe(viewLifecycleOwner) { quotesList ->
             if (!firstQuoteLoaded && quotesList.isNotEmpty()) {
-                binding.detailSPQuote.text = quotesList[0].quote // Zeige den ersten Quote an
+                binding.detailSPQuote.text = quotesList[0].quote
+                binding.mrHanky.alpha = 0f
+                // Der erste Quote wird angezeigt
                 firstQuoteLoaded = true
                 Log.d("imageName004", "${quotesList[0].name}")
             }
