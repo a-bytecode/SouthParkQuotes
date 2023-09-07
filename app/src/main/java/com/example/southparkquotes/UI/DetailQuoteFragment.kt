f542e0455410f2a5a713e6d478ce638fead046be
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-09-07 16:39:49 +0200

    build: Anpassung an der Sichtbarkeit und dem Ladeverhalten der CardView im DetailQuoteFragment.kt

--- app/src/main/java/com/example/southparkquotes/UI/DetailQuoteFragment.kt
+++ app/src/main/java/com/example/southparkquotes/UI/DetailQuoteFragment.kt
@@ -38,14 +38,10 @@ class DetailQuoteFragment : Fragment() {
         return binding.root
     }
 
-    override fun onResume() {
+    override fun onResume() { // on Resume wird verwendet um Aktionen
+        // bei der wiederherstellung des Frames zu aktivieren.
         super.onResume()
-
-        // Leeren Sie den Text in Ihrer TextView
-        binding.detailSPQuote.text = ""
-
-        // Blenden Sie die CardView aus
-        binding.cardViewDetail.visibility = View.GONE
+        binding.cardViewDetail.visibility = View.INVISIBLE
         binding.mrHanky.alpha = 1f
     }
 
@@ -57,29 +53,27 @@ class DetailQuoteFragment : Fragment() {
         val imageName = args.characterName
 
         var firstQuoteLoaded = false
-
-        // TODO: Animation für Mr. Hanky als Ladebild einfügen.
+        // animation für das ImageView
         val animation = AnimationUtils.loadAnimation(requireContext(), R.anim.circle_animation)
 
         if (imageResource != 0) {
             binding.charPic01detail.setImageResource(imageResource)
         }
 
-        binding.mrHanky.alpha = 1f
         binding.mrHanky.startAnimation(animation)
 
         viewModel.apiStatus.observe(viewLifecycleOwner) {
             when (it) {
                 ApiStatus.LOADING -> {
                     binding.cardViewDetail.visibility = View.INVISIBLE
-                    binding.mrHanky.visibility = View.GONE
+                    binding.mrHanky.alpha = 1f
                 }
                 ApiStatus.START -> {
                     binding.cardViewDetail.visibility = View.VISIBLE
                     binding.mrHanky.alpha = 0f
                 }
                 ApiStatus.ERROR -> {
-                    binding.cardViewDetail.visibility = View.GONE
+                    binding.cardViewDetail.visibility = View.INVISIBLE
                     binding.mrHanky.alpha = 0f
                  }
             }
@@ -90,8 +84,6 @@ class DetailQuoteFragment : Fragment() {
             val nextQuote = viewModel.getNextQuote(binding.detailSPQuote)
             if (nextQuote != null) {
                 binding.detailSPQuote.text = nextQuote
-                binding.cardViewDetail.alpha = 1f // Setzen Sie die CardView auf sichtbar
-                binding.mrHanky.alpha = 0f
             }
             Log.d("imageName001", "${imageName}")
         }
@@ -99,10 +91,8 @@ class DetailQuoteFragment : Fragment() {
         viewModel.characterNameLiveData.observe(viewLifecycleOwner) { quotesList ->
             if (!firstQuoteLoaded && quotesList.isNotEmpty()) {
                 binding.detailSPQuote.text = quotesList[0].quote
-                binding.mrHanky.alpha = 0f
-                // Der erste Quote wird angezeigt
-                firstQuoteLoaded = true
-                Log.d("imageName004", "${quotesList[0].name}")
+                 firstQuoteLoaded = true
+                Log.d("imageName004", "${quotesList[0].quote}")
             }
         }
     }
